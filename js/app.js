4010f487c4313161a001066f79a4ff14d205b223
José Narváez <josenarvaez@utez.edu.mx>
2021-09-20 14:00:13 -0500

    modificamos ruto de sw

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var swLocation = '/20213-PWA-E-TimesUTEZ/sw.js';
 
 if(navigator.serviceWorker){
     if(url.includes('localhost')){
-        swLocation='sw.js'
+        swLocation='/sw.js'
     }
     navigator.serviceWorker.register(swLocation);
 }
